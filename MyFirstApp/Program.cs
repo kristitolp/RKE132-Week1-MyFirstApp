a74c9cf812d2310dfd4a5ee9b6f7ecf0cdf159b6
kristitolp <kristi.tolp@tktk.ee>
2024-04-23 17:22:10 +0300

    create neww app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// This is a comment to my code
+
+// rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+
+using System.Net.Security;
+
+Console.WriteLine("Enter your name:"); //Output
+//String=sõne
+string userName = Console.ReadLine();//Output
+
+Console.WriteLine("Hello " + "," + userName + "!"); //Output
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !"); //Output
